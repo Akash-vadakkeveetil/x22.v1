ef321144860360a43b252e000366b19c14662e92
Akash Vadakkeveetil <akashvadakkeveetil@gmail.com>
2024-01-25 10:59:08 +0530

    The website is been repaired

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 
 function revealToSpan() {
     document.querySelectorAll(".reveal").forEach(function(elem) {
-        let spanParent = document.createElement("span"); // now this is the new span tag
-        let spanChild = document.createElement("span"); // <span></span> this is made here
+        var parent = document.createElement("span"); // now this is the new span tag
+        var child = document.createElement("span"); // <span></span> this is made here
 
-        spanParent.classList.add("parent"); // <span class = "parent"></span>
-        spanChild.classList.add("child");
+        parent.classList.add("parent"); // <span class = "parent"></span>
+        child.classList.add("child");
 
-        spanChild.innerHTML = elem.innerHTML; // <span class ="child">Creative</span>
-        spanParent.appendChild(spanChild); // span child now inside the span parent
+        child.innerHTML = elem.innerHTML; // <span class ="child">Creative</span>
+        parent.appendChild(child); // span child now inside the span parent
 
         // elem replaces its value with parent span
         elem.innerHTML = "";
-        elem.appendChild(spanParent);
+        elem.appendChild(parent);
     });
 }
 
 revealToSpan();
+
+
+gsap.to(".parent .child",{
+    y:"-100%",
+    duration:1,
+    delay:0.5,
+    ease:Expo.easeInOut
+})
 
 function loaderAnimation()
 {
@@ -33,15 +41,15 @@ tl
 .to("#green",{
     height : "100%",
     duration : 2,
-    delay : -2,
-    ease:Expo.easeInOut 
+    ease:Expo.easeInOut,
+    delay : -2, 
 })
 
-.to("#home",{
+.to("#white",{
     height : "100%",
-    duration : 2,
-    delay : -1.5,
-    ease:Expo.easeInOut 
+    duration : 3,
+    ease:Expo.easeInOut ,
+    delay : -2,
 })
 }
 
